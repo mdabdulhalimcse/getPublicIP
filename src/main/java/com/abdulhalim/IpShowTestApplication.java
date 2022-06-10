bf22387bd86fd513a883557e78acb8007ef002a0
Abdul Halim <mdabdulhalimcse@gmail.com>
2022-06-11 01:06:16 +0600

    get public and private ip address

---
+++ src/main/java/com/abdulhalim/IpShowTestApplication.java
@@ -0,0 +1,13 @@
+package com.abdulhalim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpShowTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpShowTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abdulhalim/IpShowTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdulhalim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpShowTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
